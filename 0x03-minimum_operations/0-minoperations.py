480bb8711a2581cfb4303547b1a6db62c45ff45d
HassineMohamedAnwer <mohamedanouar.hassine@gmail.com>
2023-02-12 17:29:16 +0100

    minoperations

--- 0x03-minimum_operations/0-minoperations.py
+++ 0x03-minimum_operations/0-minoperations.py
@@ -1,4 +1,11 @@
+#!/usr/bin/python3
+"""
+Minimum Operations
+"""
+
+
 def minOperations(n):
+    """fewest number of operations needed to result in exactly n characters"""
     if n <= 0:
         return 0
 
@@ -12,4 +19,3 @@ def minOperations(n):
     operations += n
 
     return int(operations - 1)
-
