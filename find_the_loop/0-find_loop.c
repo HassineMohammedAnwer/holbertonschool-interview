a23519ca3ddbe3b16f10ec0f13e6f00df2c9e848
HassineMohammedAnwer <3677@holbertonstudents.com>
2024-11-19 15:43:47 +0100

    interview/find_loop

--- find_the_loop/0-find_loop.c
+++ find_the_loop/0-find_loop.c
@@ -13,28 +13,22 @@ listint_t *find_listint_loop(listint_t *head)
 
 		if (head == NULL || head->next == NULL)
 		return (NULL);
-
 	slow = head->next;
 	fast = head->next->next;
-
 	while (slow && fast && fast->next)
 	{
 		if (slow == fast)
 		{
 			slow = head;
-
 			while (slow != fast)
 			{
 				slow = slow->next;
 				fast = fast->next;
 			}
-
 			return (slow);
 		}
-
 		slow = slow->next;
 		fast = fast->next->next;
 	}
-
 	return (NULL);
 }
