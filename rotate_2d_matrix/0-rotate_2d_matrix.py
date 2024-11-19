57e8984509d5611da2e7858bec656b41de1b505f
HassineMohammedAnwer <3677@holbertonstudents.com>
2024-11-19 15:15:05 +0100

    interview/rotate_matrix

--- rotate_2d_matrix/0-rotate_2d_matrix.py
+++ rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -7,7 +7,7 @@ def rotate_2d_matrix(matrix):
     Prototype: def rotate_2d_matrix(matrix):
     Do not return anything. The matrix must be edited in-place.
     You can assume the matrix will have 2 dimensions and will not be empty."""
-    n = matrix.shape[1]
+    n = len(matrix[0])
     start = 0
     tmp = matrix[0][0]
     while start < n:
