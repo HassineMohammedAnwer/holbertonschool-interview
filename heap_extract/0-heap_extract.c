a57276819b8c1cdebc1809b514f4ced4125609eb
HassineMohammedAnwer <3677@holbertonstudents.com>
2024-11-19 14:21:16 +0100

    interview/

---
+++ heap_extract/0-heap_extract.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "binary_trees.h"
+
+/**
+ * heap_extract - extracts the root node of a Max Binary Heap
+ * @root: double pointer to the root of the heap
+ * Return: the value stored in the root node, or 0.
+ */
+int heap_extract(heap_t **root)
+{
+	int value;
+	return ;
+}
---
+++ infinite_multiplication/0-mul.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ * @argc: The number of command-line arguments
+ * @argv: An array of pointers to the arguments
+ *
+ * Return: 0 on success, 98 on failure
+ */
+int main(int argc, char *argv[])
+{
+	if (argc != 3)
+		print_error();
+	return (0);
+}
