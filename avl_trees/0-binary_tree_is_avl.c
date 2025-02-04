6ea08dcb93be68b7b173be00195b5de8b11946be
HassineMohammedAnwer <3677@holbertonstudents.com>
2025-02-04 17:54:44 +0100

    zrgfzhiouu

---
+++ avl_trees/0-binary_tree_is_avl.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include "binary_trees.h"
+
+
+/**
+ * t_height - Measures height of a binary tree
+ *
+ * @tree: Pointer to the node to measures the height
+ * Return: The height of the tree starting at @node
+ */
+static size_t t_height(const binary_tree_t *tree)
+{
+	size_t height_l;
+	size_t height_r;
+
+	height_l = tree->left ? 1 + t_height(tree->left) : 0;
+	height_r = tree->right ? 1 + t_height(tree->right) : 0;
+	return (height_l > height_r ? height_l : height_r);
+}
+
+/**
+ * binary_tree_print - Prints binary tree
+ *
+ * @tree: Pointer to root node of the tree
+ */
+void binary_tree_print(const binary_tree_t *tree)
+{
+	char **s;
+	size_t height, i, j;
+
+	if (!tree)
+		return;
+	height = t_height(tree);
+	s = malloc(sizeof(*s) * (height + 1));
+	if (!s)
+		return;
+	for (i = 0; i < height + 1; i++)
+	{
+		s[i] = malloc(sizeof(**s) * 255);
+		if (!s[i])
+			return;
+		memset(s[i], 32, 255);
+	}
+	print_t(tree, 0, 0, s);
+	for (i = 0; i < height + 1; i++)
+	{
+		for (j = 254; j > 1; --j)
+		{
+			if (s[i][j] != ' ')
+				break;
+			s[i][j] = '\0';
+		}
+		printf("%s\n", s[i]);
+		free(s[i]);
+	}
+	free(s);
+}
