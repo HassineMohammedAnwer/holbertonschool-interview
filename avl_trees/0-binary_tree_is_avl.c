f2a45ce1bff4b9ab886e779924dde5707da1be67
HassineMohammedAnwer <3677@holbertonstudents.com>
2025-02-04 18:04:39 +0100

    zrgfzhiouu

--- avl_trees/0-binary_tree_is_avl.c
+++ avl_trees/0-binary_tree_is_avl.c
@@ -13,7 +13,7 @@
  *
  * Return: length of printed tree after process
  */
-static int print_t(const binary_tree_t *tree, int offset, int depth, char **s)
+int print_t(const binary_tree_t *tree, int offset, int depth, char **s)
 {
 	char b[6];
 	int width, left, right, is_left, i;
