167bcb3b5b8870feb288bc797d1a86763c57c026
HassineMohammedAnwer <3677@holbertonstudents.com>
2025-02-04 13:47:56 +0100

    making_change

--- making_change/0-making_change.py
+++ making_change/0-making_change.py
@@ -11,7 +11,8 @@ def makeChange(coins, total):
     If total cannot be met by any number of coins you have, return -1
     coins is a list of the values of the coins in your possession
     The value of a coin will always be an integer greater than 0
-    You can assume you have an infinite number of each denomination of coin in list
+    You can assume you have an infinite number of each denomination
+    __of coin in the list
     Your solution’s runtime will be evaluated in this task"""
     if total <= 0:
         return 0
