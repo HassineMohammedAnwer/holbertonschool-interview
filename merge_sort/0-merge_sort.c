ab8e92beeea0713cb0013cf7378f0657b6d8951f
HassineMohammedAnwer <3677@holbertonstudents.com>
2025-03-24 15:42:53 +0000

    merge_sort_update_style

--- merge_sort/0-merge_sort.c
+++ merge_sort/0-merge_sort.c
@@ -10,32 +10,32 @@
  */
 void merge(int *array, int *tmp, size_t l, size_t m, size_t r)
 {
-    size_t a = l;
-    size_t b = m + 1;
-    size_t c = l;
+	size_t a = l;
+	size_t b = m + 1;
+	size_t c = l;
 
-    printf("Merging...\n");
-    printf("[left]: ");
-    print_array(array + l, m - l + 1);
-    printf("[right]: ");
-    print_array(array + m + 1, r - m);
+	printf("Merging...\n");
+	printf("[left]: ");
+	print_array(array + l, m - l + 1);
+	printf("[right]: ");
+	print_array(array + m + 1, r - m);
 
-    while (a <= m && b <= r)
-    {
-        if (array[a] <= array[b])
-            tmp[c++] = array[a++];
-        else
-            tmp[c++] = array[b++];
-    }
+	while (a <= m && b <= r)
+	{
+		if (array[a] <= array[b])
+			tmp[c++] = array[a++];
+		else
+			tmp[c++] = array[b++];
+	}
 
-    while (a <= m)
-        tmp[c++] = array[a++];
-    while (b <= r)
-        tmp[c++] = array[b++];
-    for (c = l; c <= r; c++)
-        array[c] = tmp[c];
-    printf("[Done]: ");
-    print_array(array + l, r - l + 1);
+	while (a <= m)
+		tmp[c++] = array[a++];
+	while (b <= r)
+		tmp[c++] = array[b++];
+	for (c = l; c <= r; c++)
+		array[c] = tmp[c];
+	printf("[Done]: ");
+	print_array(array + l, r - l + 1);
 }
 
 /**
@@ -47,16 +47,16 @@ void merge(int *array, int *tmp, size_t l, size_t m, size_t r)
  */
 void merge_sort_r(int *array, int *tmp, size_t l, size_t r)
 {
-    if (l >= r)
-        return;
+	if (l >= r)
+		return;
 
-    size_t n = r - l + 1;
-    size_t l_size = n / 2;
-    size_t m = l + l_size - 1;
+	size_t n = r - l + 1;
+	size_t l_size = n / 2;
+	size_t m = l + l_size - 1;
 
-    merge_sort_r(array, tmp, l, m);
-    merge_sort_r(array, tmp, m + 1, r);
-    merge(array, tmp, l, m, r);
+	merge_sort_r(array, tmp, l, m);
+	merge_sort_r(array, tmp, m + 1, r);
+	merge(array, tmp, l, m, r);
 }
 
 /**
@@ -67,13 +67,13 @@ void merge_sort_r(int *array, int *tmp, size_t l, size_t r)
  */
 void merge_sort(int *array, size_t size)
 {
-    int *tmp;
+	int *tmp;
 
-    if (size < 2 || array == NULL)
-        return;
-    tmp = malloc(sizeof(int) * size);
-    if (tmp == NULL)
-        return;
-    merge_sort_r(array, tmp, 0, size - 1);
-    free(tmp);
+	if (size < 2 || array == NULL)
+		return;
+	tmp = malloc(sizeof(int) * size);
+	if (tmp == NULL)
+		return;
+	merge_sort_r(array, tmp, 0, size - 1);
+	free(tmp);
 }
